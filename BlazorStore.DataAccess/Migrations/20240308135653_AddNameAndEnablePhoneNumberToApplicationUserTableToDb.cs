e3d9eafadbcbcdfd6fc154e32df1e0d20956e9dc
Janus Developer <dev@retrievo.net>
2024-03-08 11:03:05 -0400

    add name and phoneumber to registration and to general user info

---
+++ BlazorStore.DataAccess/Migrations/20240308135653_AddNameAndEnablePhoneNumberToApplicationUserTableToDb.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorStore.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNameAndEnablePhoneNumberToApplicationUserTableToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
